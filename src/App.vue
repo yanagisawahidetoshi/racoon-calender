98fcf98ca9b86da308bb0894c1277e618ccdfac4
joko.ippei <joko.ippei@raccoon.ne.jp>
2024-10-09 16:23:21 +0900

    20241010 カレンダー課題

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>カレンダー</h1>
+    <section>
+      <h2>まずは、ハードコードで1日〜31日までをv-forで表示させてみる</h2>
+      <ol>
+        <li v-for="day in 31" :key="day">
+          {{ day }}日{{ applyDayOfWeek(day) }}曜日
+        </li>
+      </ol>
+    </section>
+    <section>
+      <h2>date-fnsを使って当月の1日〜最終日までをv-forで表示させる</h2>
+      <ol>
+        <li v-for="(day, index) in currentMonth" :key="index">
+          {{ day }}
+        </li>
+      </ol>
+    </section>
+  </div>
 </template>
 
 <script>
+import { eachDayOfInterval, startOfMonth, endOfMonth, format } from "date-fns";
+import { ja } from "date-fns/locale";
+
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      currentMonth: this.getCurrentMonth(),
+    };
+  },
+  methods: {
+    applyDayOfWeek(day) {
+      const dayOfWeek = ["月", "火", "水", "木", "金", "土", "日"];
+      return dayOfWeek[(day - 1) % 7];
+    },
+    getCurrentMonth() {
+      const now = new Date();
+      const dates = eachDayOfInterval({
+        start: startOfMonth(now),
+        end: endOfMonth(now),
+      });
+      return dates.map((date) => {
+        return format(date, "yyyy年MMMMdo（EEEE）", { locale: ja });
+      });
+    },
+  },
 };
 </script>
