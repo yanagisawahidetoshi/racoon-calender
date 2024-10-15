5afb3bf36cef3bfe411b147d3e0c239d41b89748
sakashita_yoshiyuki <sakashita.yoshiyuki@raccoon.ne.jp>
2024-10-15 09:52:16 +0900

    カレンダー　日付・曜日

--- src/App.vue
+++ src/App.vue
@@ -6,34 +6,35 @@
         {{ n.DAY }}({{ n.DOW }})
       </li>
     </ol>
-
-    月初：{{ BOM }} 月末：{{ EOM }}
   </div>
 </template>
 
 <script>
-import { startOfMonth, endOfMonth, getDay, format, addDays } from "date-fns";
+import {
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  format,
+  getDay,
+} from "date-fns";
 
 export default {
   name: "app",
   components: {},
   data() {
     const currentDate = new Date();
-    const start = startOfMonth(currentDate);
-    const end = endOfMonth(currentDate);
+    const SOM = startOfMonth(currentDate);
+    const LOM = endOfMonth(currentDate);
     // 曜日追加
     const daysOfWeek = ["日", "月", "火", "水", "木", "金", "土"];
-    const calendar = []; // 空の配列
-    for (let i = 0; i < end.getDate(); i++) {
-      const day = addDays(start, i);
-      calendar.push({
-        DAY: format(day, "dd"), // 01とするためのformat
-        DOW: daysOfWeek[getDay(day)], // 曜日
-      });
-    }
+    const calendar = eachDayOfInterval({
+      start: new Date(SOM),
+      end: new Date(LOM),
+    }).map((day) => ({
+      DAY: format(day, "dd"),
+      DOW: daysOfWeek[getDay(day)],
+    }));
     return {
-      BOM: start.getDate(), // 月初
-      EOM: end.getDate(), // 月末
       calendar,
     };
   },
