09ad7f31b7dfbbbe5eccc01a22a8a1e6ab281d5b
kobayashi.tetsuya <kobayashi.tetsuya@raccoon.ne.jp>
2024-10-15 10:26:36 +0900

    1015 前月、翌月、当月ボタン実装

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,10 @@
   <div id="app">
     <h1>カレンダー</h1>
     <section>
-      <h2>ハードコードで1日〜31日までをv-forで表示</h2>
-      <ol>
-        <li v-for="index in 31" :key="index">{{ index }}日</li>
-      </ol>
-    </section>
-    <section>
-      <h2>date-fnsを使って当月の1日〜最終日までをv-forで表示</h2>
+      <h2>来月、翌月ボタン追加</h2>
+      <button @click="prevMonth">前月</button>
+      <button @click="currentMonth">当月</button>
+      <button @click="nextMonth">翌月</button>
       <ol>
         <li v-for="(date, index) in dateList" :key="index">
           {{ formatDate(date) }}
@@ -19,12 +16,13 @@
 </template>
 
 <script>
-import { startOfMonth, endOfMonth, eachDayOfInterval, format } from "date-fns";
+import { startOfMonth, endOfMonth, eachDayOfInterval, format, addMonths, subMonths } from "date-fns";
 import ja from "date-fns/locale/ja";
 export default {
   name: "App",
   data() {
     return {
+      currentDay : new Date(),
       dateList: [],
     };
   },
@@ -36,13 +34,23 @@ export default {
       return format(date, "yyyy年M月d日（E）", { locale: ja })
     },
     generateDate() {
-      const now = new Date();
-      const start = startOfMonth(now);
-      const end = endOfMonth(now);
+      const start = startOfMonth(this.currentDay);
+      const end = endOfMonth(this.currentDay);
       this.dateList = eachDayOfInterval({ start, end });
     },
+    prevMonth() {
+      this.currentDay = subMonths(this.currentDay, 1);
+      this.generateDate();
+    },
+    nextMonth() {
+      this.currentDay = addMonths(this.currentDay, 1);
+      this.generateDate();
+    },
+    currentMonth() {
+      this.currentDay = new Date();
+      this.generateDate();
+    }
   },
-  components: {},
 };
 </script>
 <style scoped>
