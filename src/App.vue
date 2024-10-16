edc57be8ccc3568b77463be1af3ae60232cb4c82
sakashita_yoshiyuki <sakashita.yoshiyuki@raccoon.ne.jp>
2024-10-16 15:36:35 +0900

    calendar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <Button @click="previousMonth()" class="btn">前月</Button>
+    <span>{{ dispDate }}</span>
+    <Button @click="nexttMonth()" class="btn">次月</Button>
+    <Button @click="currentMonth()" class="btn">当月</Button>
     {{ /* 日付と曜日を出力 */ }}
     <ol>
       <li v-for="(n, index) in calendar" :key="index">
@@ -10,20 +14,65 @@
 </template>
 
 <script>
-// 表示年月を表示
-// 左右に前月次月を切り替えるボタンを表示
-// 当月ボタンを表示
+// 1.初回表示時は当月日がを表示する
+// 2.年月用の変数を用意（入れ替えることで、月初、月末の取得を動的に行う）
+// 3.date-fnsに年月の算出が出来る関数があれば利用、なければ計算用のロジックを考える。
+// 4.当月・次月・前月のボタンで、年月の変数を切り替える（値を入れ替える？）
+//   ボタンは使いまわしが出来そうなのでコンポーネント化
+// 5.日・曜日は再算出（関数利用）用の、computedを用意
+//   表示用の年月もここで処理
+// 6.ボタン操作用のmethodsを用意
+// 7.dateに、年月日用の変数を定義。日本語用の曜日を定義
 import {
   startOfMonth,
   endOfMonth,
   eachDayOfInterval,
   format,
   getDay,
+  addMonths,
 } from "date-fns";
+import { Button } from "./components/Button/";
 
 export default {
   name: "app",
-  components: {},
+  components: {
+    Button,
+  },
+
+  data() {
+    return {
+      currentDate: new Date(),
+      daysOfWeek: ["日", "月", "火", "水", "木", "金", "土"],
+    };
+  },
+  computed: {
+    dispDate() {
+      return format(this.currentDate, "yyyy年MM月");
+    },
+    calendar() {
+      const SOM = startOfMonth(this.currentDate);
+      const EOM = endOfMonth(this.currentDate);
+      return eachDayOfInterval({
+        start: new Date(SOM),
+        end: new Date(EOM),
+      }).map((days) => ({
+        date: format(days, "dd日"),
+        day: this.daysOfWeek[getDay(days)],
+      }));
+    },
+  },
+  methods: {
+    previousMonth() {
+      this.currentDate = addMonths(this.currentDate, -1);
+    },
+    nexttMonth() {
+      this.currentDate = addMonths(this.currentDate, 1);
+    },
+    currentMonth() {
+      this.currentDate = new Date();
+    },
+  },
+  /*
   data() {
     const currentDate = new Date();
     const SOM = startOfMonth(currentDate);
@@ -42,6 +91,7 @@ export default {
       calendar,
     };
   },
+	*/
 };
 </script>
 <style scoped>
@@ -50,5 +100,8 @@ li {
   padding: 0;
   margin: 0;
   list-style: none;
+}
+.btn {
+  margin: 0 6px;
 }
 </style>
---
+++ src/components/Button/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <component
+    :is="tagName"
+    v-bind:type="tagName === 'button' ? 'button' : null"
+    @click="$emit('click')"
+    class="btn"
+    ><slot></slot
+  ></component>
+</template>
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    tagName: {
+      type: String,
+      default: "button",
+      validator(value) {
+        return ["button", "a", "div", "span"].includes(value);
+      },
+    },
+    type: {
+      type: String,
+      required: false, //button以外の場合もあるため
+      default: "button",
+    },
+  },
+};
+</script>
+<style scoped>
+.btn {
+  padding: 6px 12px;
+  border: 1px solid #333;
+  border-radius: 6px;
+}
+</style>
