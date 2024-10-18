f96fc28a18b7d3c60461bd7cf09a1451e807c703
tanaka-eri <tanakaeri888@gmail.com>
2024-10-18 19:11:36 +0900

    10/8に出された宿題

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ol>
+      <li v-for="date in formattedDates" :key="date">
+        <p>{{ date }}</p>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
+import {
+  startOfMonth,
+  lastDayOfMonth,
+  eachDayOfInterval,
+  format,
+} from "date-fns";
 export default {
   name: "App",
   components: {},
+  computed: {
+    dates() {
+      const now = new Date();
+      const start = startOfMonth(now);
+      const end = lastDayOfMonth(now);
+      return eachDayOfInterval({ start, end });
+    },
+    formattedDates() {
+      return this.dates.map((date) => {
+        return format(date, "dd eeee");
+      });
+    },
+  },
 };
 </script>
