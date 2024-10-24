9a09f8fb103ba5f5aa44fe014291440659c430fa
kobayashi.tetsuya <kobayashi.tetsuya@raccoon.ne.jp>
2024-10-24 13:52:53 +0900

    1022 カレンダー　モーダル表示まで

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
   <div id="app">
-    <h1>カレンダー</h1>
-    <section class="mb10">
-      <h2>来月、翌月ボタン追加</h2>
-      <ul class="btnArea mb10">
-        <li><button class="btn" @click="prevMonth">前月</button></li>
+    <header class="header">
+      <ul class="btnArea">
+        <li><button class="btn" @click="prevMonth">&lt;</button></li>
         <li><button class="btn" @click="currentMonth">当月</button></li>
-        <li><button class="btn" @click="nextMonth">翌月</button></li>
+        <li><button class="btn" @click="nextMonth">&gt;</button></li>
       </ul>
-      <ol>
+      <div>{{ formatMonth(activeDate) }}</div>
+      <button class="btn" @click="openModal">登録</button>
+      <!-- TODO: 登録ボタンの実装 -->
+      <vue-modal-2
+        name="scheduleRegister-modal"
+        :headerOptions="{
+          title: '予定を登録',
+        }"
+        :footerOptions="{
+          btn1: 'キャンセル',
+          btn2: '登録',
+          btn2Style: {
+            backgroundColor: 'green',
+          },
+          btn2OnClick: () => {
+            doDelete();
+          },
+          btn1OnClick: () => {
+            $vm2.close('scheduleRegister-modal');
+          },
+        }"
+        @on-close="closeModal"
+        class="scheduleRegister-modal"
+      >
+        <div class="scheduleRegister-content">
+          <div>
+            <InputText v-model="inputText" />
+          </div>
+          <div><InputDate v-model="inputDate" /></div>
+          <dl>
+            <dt>開始時間</dt>
+            <dd><InputTime v-model="inputTime" /></dd>
+          </dl>
+          <dl>
+            <dt>終了時間</dt>
+            <dd><InputTime v-model="inputTime" /></dd>
+          </dl>
+        </div>
+      </vue-modal-2>
+    </header>
+    <section>
+      <ol class="calender">
         <li v-for="(date, index) in generateDate()" :key="index">
-          {{ formatDate(date) }}
+          <div class="date-content">
+            <p class="date">{{ formatDate(date) }}</p>
+            <p class="day">{{ formatDay(date) }}</p>
+          </div>
+          <div class="schedule-content">
+            <!-- TODO: スケジュール登録 -->
+            <div><span>00:00</span> ～ <span>00:00</span></div>
+            <p>スケジュールが入ります</p>
+          </div>
         </li>
       </ol>
-    </section>
-    <section>
-      <h2>inputコンポーネント確認用</h2>
-      <div class="input-wrap">
-        <InputText v-model="inputText" />
-        <InputDate v-model="inputDate" />
-        <InputTime v-model="inputTime" />
-      </div>
     </section>
   </div>
 </template>
@@ -47,18 +86,24 @@ export default {
   data() {
     return {
       activeDate: new Date(),
-      changeText: "",
+      isModalVisible: false,
+      inputText: "",
+      inputDate: "",
+      inputTime: "",
     };
   },
   mounted() {
     this.generateDate();
   },
   methods: {
-    handleText(val) {
-      this.changeText = val;
-    },
     formatDate(date) {
-      return format(date, "yyyy年M月d日（E）");
+      return format(date, "d");
+    },
+    formatDay(date) {
+      return format(date, "E");
+    },
+    formatMonth(date) {
+      return format(date, "yyyy年M月");
     },
     generateDate() {
       const start = startOfMonth(this.activeDate);
@@ -74,6 +119,14 @@ export default {
     currentMonth() {
       this.activeDate = new Date();
     },
+    closeModal() {
+      this.isModalVisible = false;
+      this.$vm2.close("scheduleRegister-modal");
+    },
+    openModal() {
+      this.isModalVisible = true;
+      this.$vm2.open("scheduleRegister-modal");
+    },
   },
 };
 </script>
@@ -81,8 +134,16 @@ export default {
 .mb10 {
   margin-bottom: 10px;
 }
+.header {
+  padding: 10px;
+  border-bottom: 2px solid #333;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
 .btnArea {
   display: flex;
+  align-items: center;
   gap: 10px;
 }
 .btn {
@@ -90,8 +151,36 @@ export default {
   background-color: #ddd;
   border-radius: 3px;
 }
-.input-wrap {
+.calender > li {
+  padding: 20px;
+  border-bottom: 2px solid #ccc;
   display: flex;
+  gap: 15px;
+}
+.date-content {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+.date-content .day {
+  font-size: 12px;
+}
+.schedule-content {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.scheduleRegister-content {
+  padding: 0 20px 0.6rem;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
   gap: 10px;
+}
+.scheduleRegister-content dl {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
 }
 </style>
