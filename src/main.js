d4a308018b4585664d63bdfbbdc3d9e1a02ab88c
tsukahara aya <tsukahara.aya@raccoon.ne.jp>
2024-10-28 17:51:43 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "destyle.css";
-import Router from "vue-router";
 
 import Modal from "@burhanahmeed/vue-modal-2";
 
 Vue.use(Modal);
-Vue.use(Router);
 Vue.config.productionTip = false;
 
-// const router = new Router({});
-
 new Vue({
-  // router,
   render: (h) => h(App),
 }).$mount("#app");
