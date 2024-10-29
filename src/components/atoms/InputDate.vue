cee7d5962327f1779410591cb7973f5892fe8cb0
tanaka-eri <tanakaeri888@gmail.com>
2024-10-29 12:18:13 +0900

    10/15の課題

---
+++ src/components/atoms/InputDate.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="date"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: "InputDate",
+  components: {},
+  props: {
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
---
+++ src/components/atoms/InputTime.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="time"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: "InputTime",
+  components: {},
+  props: {
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
