f37dbdc19af247b4b2f71eea5a8075b3a4e4ea45
tanaka-eri <tanakaeri888@gmail.com>
2024-10-29 12:45:15 +0900

    15日の課題テスト済

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,65 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <button type="submit" @click="changeMonth(-1)">前月</button>
+    <button type="submit" @click="goThisMonth">当月</button>
+    <button type="submit" @click="changeMonth(1)">翌月</button>
+    <ol>
+      <li v-for="(date, index) in dates" :key="index">
+        <p>{{ format(date, "MM/dd EE") }}</p>
+      </li>
+    </ol>
+    {{ inputDate }}
+    {{ inputTime }}
+    {{ inputText }}<br />
+    <InputDate v-model="inputDate" />
+    <InputTime v-model="inputTime" />
+    <InputText v-model="inputText" />
+  </div>
 </template>
 
 <script>
+import {
+  startOfMonth,
+  lastDayOfMonth,
+  eachDayOfInterval,
+  format,
+  addMonths,
+} from "date-fns";
+import InputDate from "./components/atoms/InputDate";
+import InputText from "./components/atoms/InputText";
+import InputTime from "./components/atoms/InputTime";
 export default {
   name: "App",
-  components: {},
+  components: {
+    InputDate,
+    InputText,
+    InputTime,
+  },
+  data() {
+    return {
+      currentDay: new Date(),
+      inputDate: "2024-10-28",
+      inputText: "aaa",
+      inputTime: "10:12",
+    };
+  },
+  computed: {
+    dates() {
+      const start = startOfMonth(this.currentDay);
+      const end = lastDayOfMonth(this.currentDay);
+      return eachDayOfInterval({ start, end });
+    },
+  },
+  methods: {
+    format(date, pattern) {
+      return format(date, pattern);
+    },
+    changeMonth(num) {
+      this.currentDay = addMonths(this.currentDay, num);
+    },
+    goThisMonth() {
+      this.currentDay = new Date();
+    },
+  },
 };
 </script>
