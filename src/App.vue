3dd4b1963c330309ccd15343864f4554b0c2f64f
oi.tomoharu <oi.tomoharu@raccoon.ne.jp>
2024-10-31 17:33:30 +0900

    10月8日分　日付表示

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ol>
+      <li v-for="(date, index) in dates" :key="index">
+        <p>{{ date }}</p>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
+import { startOfMonth, eachDayOfInterval, lastDayOfMonth, format } from 'date-fns';
+
 export default {
   name: "App",
   components: {},
+  data() {
+    return{
+      today: new Date(),
+    }
+  },
+  computed: {
+    dates(){
+      const startDate = startOfMonth(this.today);
+      const lastDate = lastDayOfMonth(this.today);
+      return eachDayOfInterval({
+        start: startDate,
+        end: lastDate
+      }).map(date => format(date, 'dd EEEE'))
+    }
+  }
 };
 </script>
