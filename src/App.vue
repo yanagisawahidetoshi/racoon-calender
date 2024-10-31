29e8d67fcf9310c02d3efa45f5195426931d4a6e
oi.tomoharu <oi.tomoharu@raccoon.ne.jp>
2024-10-31 18:05:44 +0900

    10月10日分　先月・翌月・当月ボタン

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <ul>
+      <li><button @click="movePreviousMonth">prev</button></li>
+      <li><button @click="moveCurrentMonth">current</button></li>
+      <li><button @click="moveNextMonth">next</button></li>
+    </ul>
     <ol>
       <li v-for="(date, index) in dates" :key="index">
         <p>{{ date }}</p>
@@ -9,7 +14,7 @@
 </template>
 
 <script>
-import { startOfMonth, eachDayOfInterval, lastDayOfMonth, format } from 'date-fns';
+import { startOfMonth, eachDayOfInterval, lastDayOfMonth, format, addMonths } from 'date-fns';
 
 export default {
   name: "App",
@@ -26,8 +31,19 @@ export default {
       return eachDayOfInterval({
         start: startDate,
         end: lastDate
-      }).map(date => format(date, 'dd EEEE'))
+      }).map(date => format(date, 'MM/dd EEEE'))
     }
+  },
+  methods: {
+    movePreviousMonth() {
+      return this.today = addMonths(this.today, -1);
+    },
+    moveCurrentMonth() {
+      return this.today = new Date();
+    },
+    moveNextMonth() {
+      return this.today = addMonths(this.today, 1);
+    },
   }
 };
 </script>
