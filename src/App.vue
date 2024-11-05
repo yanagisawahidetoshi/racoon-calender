4bd05a5c98d9ff8391d2e1227aae3398e85aecd1
joko.ippei <joko.ippei@raccoon.ne.jp>
2024-11-05 11:35:30 +0900

    20241105 カレンダー課題 コンポーネント分割 レビュー指摘対応01

--- src/App.vue
+++ src/App.vue
@@ -12,38 +12,6 @@
         </li>
       </ol>
     </section>
-    <vue-modal-2
-      @on-close="$vm2.close('registScheduleModal')"
-      name="registScheduleModal"
-      :headerOptions="{
-        title: '予定を登録',
-      }"
-      :footerOptions="{
-        btn1: 'キャンセル',
-        btn2: '登録',
-        btn2Style: {
-          backgroundColor: 'green',
-        },
-        btn1OnClick: () => {
-          $vm2.close('registScheduleModal');
-        },
-      }"
-    >
-      <div class="wrap_input">
-        <dl class="input_col">
-          <dt class="input_title">日付</dt>
-          <dd><InputDate v-model="dateValue" /></dd>
-        </dl>
-        <dl class="input_col">
-          <dt class="input_title">開始時間</dt>
-          <dd><InputTime v-model="startTimeValue" /></dd>
-        </dl>
-        <dl class="input_col">
-          <dt class="input_title">終了時間</dt>
-          <dd><InputTime v-model="endTimeValue" /></dd>
-        </dl>
-      </div>
-    </vue-modal-2>
   </div>
 </template>
 
@@ -56,14 +24,12 @@ import {
   addMonths,
   parse,
 } from "./libs/date-util";
-import InputDate from "./components/atoms/InputDate.vue";
-import InputTime from "./components/atoms/InputTime.vue";
 import CalenderHeader from "./components/molecules/Calender/Header.vue";
 import CalenderDate from "./components/molecules/Calender/Date.vue";
 
 export default {
   name: "App",
-  components: { InputDate, InputTime, CalenderHeader, CalenderDate },
+  components: { CalenderHeader, CalenderDate },
   data() {
     return {
       active: new Date(),
@@ -125,19 +91,5 @@ export default {
 }
 .list .detail {
   margin-bottom: 8px;
-}
-.wrap_input {
-  margin-bottom: 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 0 10px;
-}
-.input_col {
-  display: flex;
-  align-items: center;
-}
-.input_title:after {
-  content: "：";
 }
 </style>
--- src/components/molecules/Calender/Header.vue
+++ src/components/molecules/Calender/Header.vue
@@ -7,14 +7,48 @@
       <BaseButton @click="$vm2.open('registScheduleModal')">登録</BaseButton>
       <!--vue-modal-2はグローバルに提供されている-->
     </div>
+    <vue-modal-2
+      @on-close="$vm2.close('registScheduleModal')"
+      name="registScheduleModal"
+      :headerOptions="{
+        title: '予定を登録',
+      }"
+      :footerOptions="{
+        btn1: 'キャンセル',
+        btn2: '登録',
+        btn2Style: {
+          backgroundColor: 'green',
+        },
+        btn1OnClick: () => {
+          $vm2.close('registScheduleModal');
+        },
+      }"
+    >
+      <div class="wrap_input">
+        <dl class="input_col">
+          <dt class="input_title">日付</dt>
+          <dd><InputDate v-model="dateValue" /></dd>
+        </dl>
+        <dl class="input_col">
+          <dt class="input_title">開始時間</dt>
+          <dd><InputTime v-model="startTimeValue" /></dd>
+        </dl>
+        <dl class="input_col">
+          <dt class="input_title">終了時間</dt>
+          <dd><InputTime v-model="endTimeValue" /></dd>
+        </dl>
+      </div>
+    </vue-modal-2>
   </header>
 </template>
 
 <script>
 import BaseButton from "@/components/atoms/BaseButton.vue";
+import InputDate from "@/components/atoms/InputDate.vue";
+import InputTime from "@/components/atoms/InputTime.vue";
 export default {
   name: "CalenderHeader",
-  components: { BaseButton },
+  components: { BaseButton, InputDate, InputTime },
   props: {
     changeDate: () => {},
     changeCurrentMonth: () => {},
@@ -32,5 +66,19 @@ export default {
   display: flex;
   gap: 12px;
   margin-bottom: 20px;
+}
+.wrap_input {
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 0 10px;
+}
+.input_col {
+  display: flex;
+  align-items: center;
+}
+.input_title:after {
+  content: "：";
 }
 </style>
