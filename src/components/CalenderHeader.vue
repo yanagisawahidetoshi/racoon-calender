5a3941379236ea896fef99cff266701901fb7bff
joko.ippei <joko.ippei@raccoon.ne.jp>
2024-12-03 14:33:38 +0900

    12/3 講義 モーダル途中まで

--- src/components/CalenderHeader.vue
+++ src/components/CalenderHeader.vue
@@ -11,22 +11,36 @@
       <li>
         <button type="button" @click="$emit('changeToNextMonth')">翌月</button>
       </li>
+      <li>
+        <button type="button" @click="openScheduleModal">登録</button>
+      </li>
     </ul>
+    <ScheduleModal />
   </div>
 </template>
 
 <script>
 import { format } from "../libs/date-fns";
+import ScheduleModal from "../components/ScheduleModal.vue";
 
 export default {
   name: "CalenderHeader",
   props: {
     currentDate: { type: Date },
   },
+  components: { ScheduleModal },
   computed: {
     title() {
       console.log(this.currentDate);
       return format(this.currentDate, "yyyy年MM月");
+    },
+  },
+  methods: {
+    openScheduleModal() {
+      this.$vm2.open("scheduleModal");
+    },
+    close() {
+      this.$vm2.close("scheduleModal");
     },
   },
 };
---
+++ src/components/ScheduleModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <vue-modal-2 name="scheduleModal"> test </vue-modal-2>
+</template>
+
+<script>
+export default {
+  name: "ScheduleModal",
+};
+</script>
