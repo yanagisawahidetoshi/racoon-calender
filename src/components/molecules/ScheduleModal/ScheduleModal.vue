2b1eff3338ec6a1a1957bd66bdd001fb4bf102d9
oi.tomoharu <oi.tomoharu@raccoon.ne.jp>
2024-12-09 15:08:56 +0900

    name定数化

--- src/components/molecules/ScheduleModal/ScheduleModal.vue
+++ src/components/molecules/ScheduleModal/ScheduleModal.vue
@@ -1,9 +1,10 @@
 <template>
-  <vue-modal-2 name="scheduleModal" @on-close="$emit('close')"> test 
+  <vue-modal-2 :name='modalName' @on-close="$emit('close')"> test 
   </vue-modal-2>
 </template>
 
 <script>
+const MODAL_NAME = 'scheduleModal';
 export default {
   name: "ScheduleModal",
   props: {
@@ -12,12 +13,17 @@ export default {
       required: true,
     },
   },
+  data() {
+    return{
+      modalName: MODAL_NAME,
+    }
+  },
   watch: {
     isModalOpen: function(flag) {
-      if (flag === true) {
-        this.$vm2.open("scheduleModal");
+      if (flag) {
+        this.$vm2.open(MODAL_NAME);
       }else{
-        this.$vm2.close("scheduleModal");
+        this.$vm2.close(MODAL_NAME);
       }
     }
   },
