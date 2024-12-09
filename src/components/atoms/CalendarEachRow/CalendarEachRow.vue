2063bdd4b038b7b38a06c8e13dc822d0e5ab55f4
oi.tomoharu <oi.tomoharu@raccoon.ne.jp>
2024-12-09 11:16:53 +0900

    コンポーネント分割

---
+++ src/components/atoms/CalendarEachRow/CalendarEachRow.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ formatDate(date, 'MM/dd EEEE') }}</p>
+</template>
+
+<script>
+import {
+  format,
+} from "@/libs/date-fns";
+
+export default {
+  name: "CalendarEachRow",
+  props: {
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  methods: {
+    formatDate(date, setting){
+      return format(date, setting);
+    },
+  },
+}
+</script>
---
+++ src/components/molecules/CalendarHeader/CalendarHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <ul>
+      <li><button @click="$emit('changeMonth', -1)">prev</button></li>
+      <li><button @click="$emit('changeToCurrentMonth')">current</button></li>
+      <li><button @click="$emit('changeMonth', 1)">next</button></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "CalendarHeader",
+  props: {
+    baseDate: {
+      type: Date,
+      required: true,
+    },
+  },
+}
+</script>
