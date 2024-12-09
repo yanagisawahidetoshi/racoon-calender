fd33f5f2b74e41feb4390bcaa56046fc9c5a6587
oi.tomoharu <oi.tomoharu@raccoon.ne.jp>
2024-12-09 13:49:25 +0900

    モーダル仮

--- src/components/molecules/CalendarHeader/CalendarHeader.vue
+++ src/components/molecules/CalendarHeader/CalendarHeader.vue
@@ -4,17 +4,40 @@
       <li><button @click="$emit('changeMonth', -1)">prev</button></li>
       <li><button @click="$emit('changeToCurrentMonth')">current</button></li>
       <li><button @click="$emit('changeMonth', 1)">next</button></li>
+      <li>
+        <button type="button" @click="openScheduleModal">登録</button>
+      </li>
     </ul>
+    <ScheduleModal
+      :isModalOpen="isModalOpen"
+      @close="closeScheduleModal" />
   </header>
 </template>
 
 <script>
+import ScheduleModal from "../ScheduleModal/ScheduleModal.vue";
 export default {
   name: "CalendarHeader",
+  components: { ScheduleModal },
   props: {
     baseDate: {
       type: Date,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+  methods: {
+    openScheduleModal() {
+      this.isModalOpen = true;
+      this.$vm2.open("scheduleModal");
+    },
+    closeScheduleModal() {
+      this.isModalOpen = false;
+      this.$vm2.close("scheduleModal");
     },
   },
 }
---
+++ src/components/molecules/ScheduleModal/ScheduleModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <vue-modal-2 v-if="isModalOpen" name="scheduleModal"> test 
+    <!-- <button @click="$emit('close')">閉じる</button> -->
+  </vue-modal-2>
+</template>
+
+<script>
+export default {
+  name: "ScheduleModal",
+  props: {
+    isModalOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  // computed: {
+  //   modalOpening(){
+  //     モーダルが開いているかどうかを監視
+  //   },
+  // },
+};
+</script>
